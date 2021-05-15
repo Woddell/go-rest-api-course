1d5df49b0f95d469d87ac1d05c2db3d57c777722
Chris <cweir132@gmail.com>
2021-05-15 14:26:34 +0100

    implemented endpoints.

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -3,25 +3,108 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/Woddell/go-rest-api-course/internal/comment"
 	"github.com/gorilla/mux"
 )
 
 // Handler - stores pointer to our comments service
 type Handler struct {
-	Router *mux.Router
+	Router  *mux.Router
+	Service *comment.Service
 }
 
 // NewHandler - returns a pointer to a Handler
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(service *comment.Service) *Handler {
+	return &Handler{
+		Service: service,
+	}
 }
 
 // SetupRoutes - sets up all the routes for our application
 func (h *Handler) SetupRoutes() {
 	fmt.Println("Setting up routes.")
 	h.Router = mux.NewRouter()
+
+	h.Router.HandleFunc("/api/comment", h.GetAllComments).Methods("GET")
+	h.Router.HandleFunc("/api/comment", h.PostComment).Methods("POST")
+	h.Router.HandleFunc("/api/comment/{id}", h.GetComment).Methods("GET")
+	h.Router.HandleFunc("/api/comment/{id}", h.DeleteComment).Methods("DELETE")
+	h.Router.HandleFunc("/api/comment/{id}", h.UpdateComment).Methods("PUT")
+
 	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK")
 	})
+}
+
+// GetComment - retrive a comment by ID
+func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	i, err := strconv.ParseUint(id, 10, 64)
+
+	if err != nil {
+		fmt.Fprintf(w, "Unable to parse UINT from ID")
+	}
+
+	comment, err := h.Service.GetComment(uint(i))
+	if err != nil {
+		fmt.Fprintf(w, "Error Retrieving Comment By ID")
+	}
+
+	fmt.Fprintf(w, "%+v", comment)
+}
+
+// GetComments - retrieves all comments from the comment service
+func (h *Handler) GetAllComments(w http.ResponseWriter, r *http.Request) {
+	comments, err := h.Service.GetAllComments()
+	if err != nil {
+		fmt.Fprintf(w, "Failed to retrieve all comments")
+	}
+
+	fmt.Fprintf(w, "%+v", comments)
+}
+
+// PostComment - Saves comment
+func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
+	comment, err := h.Service.PostComment(comment.Comment{
+		Slug: "/",
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Failed to post new comment")
+	}
+
+	fmt.Fprintf(w, "%+v", comment)
+}
+
+// UpdateComment - Updates comment by ID
+func (h *Handler) UpdateComment(w http.ResponseWriter, r *http.Request) {
+	comment, err := h.Service.UpdateComment(1, comment.Comment{
+		Slug: "/",
+	})
+	if err != nil {
+		fmt.Fprintf(w, "Failed to update comment")
+	}
+
+	fmt.Fprintf(w, "%+v", comment)
+}
+
+// DeleteComment - Deletes a comment by ID
+func (h *Handler) DeleteComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	commentID, err := strconv.ParseUint(id, 10, 64)
+
+	if err != nil {
+		fmt.Fprintf(w, "Failed to parse uint from ID")
+	}
+
+	err = h.Service.DeleteComment(uint(commentID))
+	if err != nil {
+		fmt.Fprintf(w, "Failed to delete comment")
+	}
+
+	fmt.Fprintf(w, "Deleted Comment")
 }
